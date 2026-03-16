b9c2ebb8a1b40a51b70608437f32d076e2fc7ca7
Nick Harte <bippy@boppy.com>
2026-03-16 12:05:07 +0000

    Added Okados

--- file1.py
+++ file1.py
@@ -14,3 +14,10 @@ print("These new spoons for eating with??!")
 for i in range(3):
     print("Never!")
 print("Never using those " + "".join(["S", "P", "O", "O", "N", "S"]) + "!")
+
+for i in range(7):
+    if i > 0:
+        if i%3 == 0:
+            print("Oka-do, Oka-do, Oka-do,")
+        else:
+            print("Oka-")
