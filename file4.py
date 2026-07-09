3ecc6024ac7fdfea1eb8b479497ec5e6ad212e67
Nick Harte <bippy@boppy.com>
2026-07-09 11:16:30 +0100

    Added FACE

--- file4.py
+++ file4.py
@@ -50,4 +50,8 @@ hoy = "hoy"
 hoy = hoy.title() + "nors"
 print(hoy)
 count = "Count"
-print(count)
+face = ["e", "c", "a", "f", "n", "i", "b"]
+face_comp = ""
+for i in range(len(face)):
+    face_comp += face[(i + 1) * -1]
+print(count + " " + face_comp.title() + "!")
