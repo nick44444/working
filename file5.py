264e944364e994d50bae8d8dc40e9576d319287b
Nick Harte <bippy@boppy.com>
2026-08-10 14:46:40 +0100

    Added aat

--- file5.py
+++ file5.py
@@ -3,4 +3,5 @@ print(belper + "?")
 nope = f"No, not {belper}: " + "-".join(list(belper.upper())) + ". " + belper.upper() + "!"
 print(nope)
 att = ["A","T"]
-print(att)
+att2 = att[0] +  "".join(att)
+print(att2)
